51ec0ee91ec1a844210a1a8b3da998816331dee0
Ghanbari <ghanbari.daniel@gmail.com>
2015-09-04 18:53:04 +0430

    Fix: we can not set self category as it's parent

--- src/Anar/ContentBundle/Form/CategoryType.php
+++ src/Anar/ContentBundle/Form/CategoryType.php
@@ -2,7 +2,9 @@
 
 namespace Anar\ContentBundle\Form;
 
+use Anar\ContentBundle\Entity\Category;
 use Anar\EngineBundle\Entity\Blog;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,9 +16,15 @@ class CategoryType extends AbstractType
      */
     private $blog;
 
-    public function __construct(Blog $blog)
+    /**
+     * @var Category|null
+     */
+    private $category;
+
+    public function __construct(Blog $blog, $category=null)
     {
         $this->blog = $blog;
+        $this->category = $category;
     }
 
     /**
@@ -55,10 +63,16 @@ class CategoryType extends AbstractType
                 'property' => 'title',
                 'placeholder' => 'placeholder',
                 'query_builder' => function ($er) {
+                    /** @var QueryBuilder $qb */
                     $qb = $er->createQueryBuilder('c');
 
-                    return $qb->where($qb->expr()->eq('c.blog', '?1'))
-                        ->setParameter(1, $this->blog->getId());
+                    if (!is_null($this->category)) {
+                        $qb->where($qb->expr()->neq('c.id', ':categoryId'))
+                            ->setParameter('categoryId', $this->category->getId());
+                    }
+
+                    return $qb->andWhere($qb->expr()->eq('c.blog', ':blogId'))
+                        ->setParameter('blogId', $this->blog->getId());
                 }
             ))
         ;
